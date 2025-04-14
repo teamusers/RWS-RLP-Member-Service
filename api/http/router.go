0485be0803fc34cb3e1f6da0922d793ecf8e20b1
eric Gan <eric_gan0513@hotmail.com>
2025-04-14 12:02:08 +0800

    Update Routers

--- api/http/router.go
+++ api/http/router.go
@@ -11,24 +11,13 @@ import (
 func Routers(e *gin.RouterGroup) {
 
 	v1Group := e.Group("/v1")
-
 	v1Group.GET("/auth", v1.AuthHandler)
-
-	// Create a sub-group for "/users" with the HttpInterceptor middleware applied.
-	usersGroup := v1Group.Group("/user/register", interceptor.HttpInterceptor())
+	usersGroup := v1Group.Group("/user", interceptor.HttpInterceptor())
 	{
 		// The endpoints below will all require a valid access token.
-		usersGroup.GET("/:email/:sign_up_type", user.GetUser)
-		usersGroup.POST("", user.CreateUser)
-		//usersGroup.PUT("/:id", v1.UpdateUser)
-		//usersGroup.DELETE("/:id", v1.DeleteUser)
-	}
+		usersGroup.GET("/login/:email", user.Login)
+		usersGroup.GET("/register/:email/:sign_up_type", user.GetUser)
+		usersGroup.POST("/register", user.CreateUser)
 
-	loginGroup := v1Group.Group("/user/login", interceptor.HttpInterceptor())
-	{
-		// The endpoints below will all require a valid access token.
-		loginGroup.GET("/:email", user.Login)
-		//usersGroup.DELETE("/:id", v1.DeleteUser)
 	}
-
 }
